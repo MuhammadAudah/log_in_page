3b0e28744d1256487621a4b8667a593f8661a965
FlutLab.io <>
2022-09-12 12:49:54 +0000

    Switching to branch log_in_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,25 +16,84 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
+  _handleLogInAction() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Padding(
+        padding: EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/instagram_log_in.png",
+              width: 222.0,
+            ),
+            const SizedBox(height: 25),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                filled: true,
+                hintText: "Phone number, e-mail, or username",
+                fillColor: Colors.grey[200],
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                suffixIcon: Icon(Icons.remove_red_eye_rounded),
+                filled: true,
+                hintText: "Password",
+                fillColor: Colors.grey[200],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text(
+                  "Forget Password!",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: _handleLogInAction,
+                child: Text("Log In"),
+              ),
+            ),
+            const SizedBox(height: 22),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Or"),
+              ],
+            ),
+            const SizedBox(height: 20),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.facebook_rounded),
+              label: const Text("Continue as Muhammad Audah"),
+            ),
+          ],
         ),
       ),
     );
